ae93fc1e7b297dbb2e58bed93c916c207c3431e5
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-07-29 04:03:30 +0300

    Updates

---
+++ precache-manifest.0fd43e16f8aa6ed62ac42ce9d23746a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b82a8beafb3ba97b4b5da7b1ac3468",
+    "url": "/FKreactapp/index.html"
+  },
+  {
+    "revision": "688e42d576888cd8b8a4",
+    "url": "/FKreactapp/static/css/main.1c5d98db.chunk.css"
+  },
+  {
+    "revision": "fcc489dd44e4cede4351",
+    "url": "/FKreactapp/static/js/2.91b2a7fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FKreactapp/static/js/2.91b2a7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688e42d576888cd8b8a4",
+    "url": "/FKreactapp/static/js/main.0602ba6e.chunk.js"
+  },
+  {
+    "revision": "98f90800ada30db24868",
+    "url": "/FKreactapp/static/js/runtime-main.db6f6549.js"
+  },
+  {
+    "revision": "cb99d66e48ce2ea972d1a00812960c99",
+    "url": "/FKreactapp/static/media/Macbook.cb99d66e.png"
+  }
+]);
