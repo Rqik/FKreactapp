7b82a7d5a7674fb9d44dddb3844bff18400b7484
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-08-24 04:15:01 +0300

    '24.08v2'

---
+++ build/precache-manifest.fa06ad4a87eff41d871670af43c84ad1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9e23f02048715f01c1d1fc1df1d27f",
+    "url": "/FKreactapp/index.html"
+  },
+  {
+    "revision": "0401fdd2d152a2040eed",
+    "url": "/FKreactapp/static/css/2.30998e96.chunk.css"
+  },
+  {
+    "revision": "97fd513a16c538b24822",
+    "url": "/FKreactapp/static/css/main.2b517ba9.chunk.css"
+  },
+  {
+    "revision": "0401fdd2d152a2040eed",
+    "url": "/FKreactapp/static/js/2.93f16da6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FKreactapp/static/js/2.93f16da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fd513a16c538b24822",
+    "url": "/FKreactapp/static/js/main.9bc6ffea.chunk.js"
+  },
+  {
+    "revision": "98f90800ada30db24868",
+    "url": "/FKreactapp/static/js/runtime-main.db6f6549.js"
+  },
+  {
+    "revision": "cb99d66e48ce2ea972d1a00812960c99",
+    "url": "/FKreactapp/static/media/Macbook.cb99d66e.png"
+  }
+]);
