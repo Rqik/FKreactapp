18de179d61a58a315065091bd73021cb702a05cd
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-10-09 02:02:30 +0300

    Updates

--- precache-manifest.d56eb6bd521905f415cc4ebf8b431c60.js
+++ precache-manifest.d56eb6bd521905f415cc4ebf8b431c60.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7fb83ef0ac8f24004b98e3aa9fd862dd",
+    "revision": "ebfdd986141f0bbe28ff7ec8e746d0bf",
     "url": "/FKreactapp/index.html"
   },
   {
-    "revision": "0401fdd2d152a2040eed",
+    "revision": "1f8d8cc507705de2cc4b",
     "url": "/FKreactapp/static/css/2.30998e96.chunk.css"
   },
   {
-    "revision": "7c642fa8e0ffd3d51adc",
-    "url": "/FKreactapp/static/css/main.50eca33c.chunk.css"
+    "revision": "7cc72679b5f11a6fc24e",
+    "url": "/FKreactapp/static/css/main.a933be97.chunk.css"
   },
   {
-    "revision": "0401fdd2d152a2040eed",
-    "url": "/FKreactapp/static/js/2.93f16da6.chunk.js"
+    "revision": "1f8d8cc507705de2cc4b",
+    "url": "/FKreactapp/static/js/2.df8c740a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/FKreactapp/static/js/2.93f16da6.chunk.js.LICENSE.txt"
+    "url": "/FKreactapp/static/js/2.df8c740a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c642fa8e0ffd3d51adc",
-    "url": "/FKreactapp/static/js/main.eb032250.chunk.js"
+    "revision": "7cc72679b5f11a6fc24e",
+    "url": "/FKreactapp/static/js/main.9746327c.chunk.js"
   },
   {
     "revision": "98f90800ada30db24868",
